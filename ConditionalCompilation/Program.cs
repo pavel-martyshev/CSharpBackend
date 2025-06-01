cec67bd55074237ff5c8d185f4a25610f4357067
Pavel <pasha.martyshev@gmail.com>
2025-06-01 13:43:41 +0300

    Complited ConditionalCompilation task

---
+++ ConditionalCompilation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConditionalCompilation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region ConstantsDeclaration
+
+#if DEBUG
+            const int x = 1;
+            const int y = 2;
+#else
+            const int x = 3;
+            const int y = 4;
+#endif
+            #endregion
+
+            Console.WriteLine(x + y);
+        }
+    }
+}

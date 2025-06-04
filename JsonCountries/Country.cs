86cd5301ff9737de656796c251a6f2be7b36125d
Pavel <pasha.martyshev@gmail.com>
2025-06-04 13:45:48 +0300

    JsonCountries task completed

---
+++ JsonCountries/Country.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonCountries;
+
+public class Country(string name, string capital, double currencies)
+{
+    [JsonProperty("country")]
+    public string Name { get; set; } = name;
+
+    public string Currency { get; set; } = capital;
+
+    public double Population { get; set; } = currencies;
+}
---
+++ JsonCountries/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonCountries;
+
+internal class Program
+{
+    private static readonly string CountriesJsonPath = Path.Combine("..", "..", "..", "countries.json");
+
+    public static void Main(string[] args)
+    {
+        var countries = JsonConvert.DeserializeObject<List<Country>>(File.ReadAllText(CountriesJsonPath));
+
+        ArgumentNullException.ThrowIfNull(countries);
+
+        Console.WriteLine("Суммарная численность по всем странам:");
+        Console.WriteLine(countries.Where(c => !double.IsNaN(c.Population)).Sum(c => c.Population));
+
+        Console.WriteLine();
+
+        Console.WriteLine("Перечень всех валют:");
+        Console.WriteLine(string.Join(
+            Environment.NewLine,
+            countries.Where(c => c.Currency != "NaN").Select(c => c.Currency).Distinct().ToList()
+            )
+        );
+    }
+}

460e23fb4a440635ae34b647cf0c12ea0113dc8e
Pavel <pasha.martyshev@gmail.com>
2025-06-04 13:45:23 +0300

    Excel task completed

---
+++ Excel/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace Excel;
+
+internal class Person(string name, string lastName, int age, string phoneNumber)
+{
+    public string Name { get; } = name;
+
+    public string LastName { get; } = lastName;
+
+    public int Age { get; } = age;
+
+    public string PhoneNumber { get; } = phoneNumber;
+
+    public override string ToString()
+    {
+        return $"""
+                Name - {Name}
+                Lastname - {LastName}
+                Age - {Age}
+                Phone number - {PhoneNumber}
+                """;
+    }
+}
---
+++ Excel/Program.cs
@@ -0,0 +1,43 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Drawing;
+
+namespace Excel;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Person> persons =
+        [
+            new Person("Oleg", "Olegov", 45, "+79998887766"),
+            new Person("Ivan", "Ivanov", 23, "-67778889900"),
+            new Person("Petr", "Ogurtsov", 25, "+53330002277"),
+            new Person("Vladislav", "Ignatiev", 25, "+12223334455")
+        ];
+
+        ExcelPackage.License.SetNonCommercialOrganization("Non commercial organization");
+
+        using var package = new ExcelPackage();
+        var ws = package.Workbook.Worksheets.Add("Persons");
+
+        ws.Cells.LoadFromCollection(persons, PrintHeaders: true);
+        ws.Cells.AutoFitColumns();
+
+        var header = ws.Cells["A1:D1"];
+        header.Style.Font.Bold = true;
+        header.Style.Fill.PatternType = ExcelFillStyle.Solid;
+        header.Style.Fill.BackgroundColor.SetColor(Color.LightSteelBlue);
+
+        foreach (var columnAddress in ws.Cells)
+        {
+            columnAddress.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+            columnAddress.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+            columnAddress.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+            columnAddress.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+        }
+
+        package.SaveAs(new FileInfo("Persons.xlsx"));
+    }
+}
+

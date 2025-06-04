2425c632773c6c5188cc9d7e62fc9da387328170
Pavel <pasha.martyshev@gmail.com>
2025-06-04 19:06:39 +0300

    Unit-tets task complited

---
+++ VectorTask/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace VectorTask;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var vector1 = new Vector(6)
+        {
+            [0] = 1,
+            [1] = 2,
+            [2] = 3
+        };
+
+        Console.WriteLine($"Вектор 1: {vector1}");
+        Console.WriteLine($"Длина вектора 1: {vector1.GetLength()}");
+
+        Console.WriteLine();
+
+        Vector vector2 = new([4, 5, 6, 7, 8, 9]);
+
+        Console.WriteLine($"Вектор 2: {vector2}");
+        Console.WriteLine($"Длина вектора 2: {vector2.GetLength()}");
+
+        Console.WriteLine();
+
+        var vector3 = Vector.GetSum(vector1, vector2);
+        Console.WriteLine($"Результат сложения вектора 1 и вектора 2: {vector3}");
+
+        Console.WriteLine();
+
+        var vector4 = Vector.GetDifference(vector2, vector1);
+        Console.WriteLine($"Результат вычитания вектора 1 из вектора 2: {vector4}");
+
+        Console.WriteLine();
+
+        var dotProduct = Vector.GetDotProduct(vector1, vector2);
+        Console.WriteLine($"Скалярное произведение вектора 1 и вектора 2: {dotProduct}");
+    }
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,169 @@
+﻿namespace VectorTask;
+
+public class Vector
+{
+    private double[] _components;
+
+    public int Size => _components.Length;
+
+    public Vector(int size)
+    {
+        if (size <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), $"Size ({size}) must be greater than 0.");
+        }
+
+        _components = new double[size];
+    }
+
+    public Vector(Vector vector)
+    {
+        _components = new double[vector.Size];
+
+        Array.Copy(vector._components, _components, vector.Size);
+    }
+
+    public Vector(double[] components)
+    {
+        if (components.Length == 0)
+        {
+            throw new ArgumentException("The array cannot be empty.", nameof(components));
+        }
+
+        _components = new double[components.Length];
+        Array.Copy(components, _components, components.Length);
+    }
+
+    public Vector(int size, double[] components)
+    {
+        if (size <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), $"Size ({size}) must be greater than 0");
+        }
+
+        _components = new double[size];
+        Array.Copy(components, _components, Math.Min(components.Length, size));
+    }
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= _components.Length)
+            {
+                throw new IndexOutOfRangeException($"The index ({index}) must be greater than or equal to 0 and less than the size ({_components.Length}).");
+            }
+
+            return _components[index];
+        }
+
+        set
+        {
+            if (index < 0 || index >= _components.Length)
+            {
+                throw new IndexOutOfRangeException($"The index ({index}) must be greater than or equal to 0 and less than the size ({_components.Length}).");
+            }
+
+            _components[index] = value;
+        }
+    }
+
+    public static Vector GetSum(Vector vector1, Vector vector2)
+    {
+        Vector resultVector = new(vector1);
+        resultVector.Add(vector2);
+
+        return resultVector;
+    }
+
+    public static Vector GetDifference(Vector vector1, Vector vector2)
+    {
+        Vector resultVector = new(vector1);
+        resultVector.Subtract(vector2);
+
+        return resultVector;
+    }
+
+    public static double GetDotProduct(Vector vector1, Vector vector2)
+    {
+        var minSize = Math.Min(vector1.Size, vector2.Size);
+
+        double dotProduct = 0;
+
+        for (var i = 0; i < minSize; i++)
+        {
+            dotProduct += vector1._components[i] * vector2._components[i];
+        }
+
+        return dotProduct;
+    }
+
+    public void Add(Vector vector)
+    {
+        if (_components.Length < vector.Size)
+        {
+            Array.Resize(ref _components, vector.Size);
+        }
+
+        for (var i = 0; i < vector.Size; i++)
+        {
+            _components[i] += vector._components[i];
+        }
+    }
+
+    public void Subtract(Vector vector)
+    {
+        if (_components.Length < vector.Size)
+        {
+            Array.Resize(ref _components, vector.Size);
+        }
+
+        for (var i = 0; i < vector.Size; i++)
+        {
+            _components[i] -= vector._components[i];
+        }
+    }
+
+    public void MultiplyByScalar(double scalar)
+    {
+        for (var i = 0; i < _components.Length; i++)
+        {
+            _components[i] *= scalar;
+        }
+    }
+
+    public void Reverse() => MultiplyByScalar(-1);
+
+    public double GetLength() => Math.Sqrt(_components.Sum(component => component * component));
+
+    public override string ToString() => $"{{{string.Join(", ", _components)}}}";
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(obj, this))
+        {
+            return true;
+        }
+
+        if (obj is null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var vector = (Vector)obj;
+
+        if (_components.Length != vector.Size)
+        {
+            return false;
+        }
+
+        return !_components.Where((t, i) => t != vector._components[i]).Any();
+    }
+
+    public override int GetHashCode()
+    {
+        const int prime = 31;
+
+        return _components.Aggregate(1, (current, component) => prime * current + component.GetHashCode());
+    }
+}

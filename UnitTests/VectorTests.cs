d5da80d3da4f705203cabb83677b19e79df23b2d
Pavel <pasha.martyshev@gmail.com>
2025-06-28 12:28:07 +0300

    Updated VectorTests

--- UnitTests/VectorTests.cs
+++ UnitTests/VectorTests.cs
@@ -2,54 +2,47 @@ using VectorTask;
 
 namespace UnitTests;
 
-public class TestVector
+public class VectorTests
 {
-    [Fact]
-    public void Add_TwoVectors_ReturnsVectorWithSummedComponents()
+    [Theory]
+    [InlineData(new double[] { 1, 2 }, new double[] { 5, 6, 7 })]
+    [InlineData(new double[] { 53, 78.1, 104.54, 8003.123 }, new double[] { 64, 2.5 })]
+    public void Add_TwoVectors_ReturnsVectorWithSummedComponents(double[] components1, double[] components2)
     {
-        var correctVector = new Vector(3)
-        {
-            [0] = 6,
-            [1] = 8,
-            [2] = 7,
-        };
+        var testVector1 = new Vector(components1);
 
-        var testVector1 = new Vector(2)
-        {
-            [0] = 1,
-            [1] = 2
-        };
+        var testVector2 = new Vector(components2);
 
-        var testVector2 = new Vector(3)
+        if (components1.Length < components2.Length)
         {
-            [0] = 5,
-            [1] = 6,
-            [2] = 7
-        };
+            Array.Resize(ref components1, components2.Length);
+        }
 
+        for (var i = 0; i < components2.Length; i++)
+        {
+            components1[i] += components2[i];
+        }
+
+        var correctVector = new Vector(components1);
         testVector1.Add(testVector2);
 
         Assert.True(correctVector.Equals(testVector1));
     }
 
     [Theory]
-    [InlineData(5, 6, 7, 2.5)]
-    [InlineData(0.42, 17.623, 24, 3.69)]
-    public void MultiplyByScalar_VectorMultiplyScalar_ReturnsMultipliedVector(double component1, double component2, double component3, double scalar)
+    [InlineData(new double[] { 5, 6, 7 }, 2.5)]
+    [InlineData(new double[] { 0.42, 17.623, 24 }, 3.69)]
+    [InlineData(new double[] { 0.2, 321.43, 1, 24.42, 6.75 }, 1.44)]
+    public void MultiplyByScalar_VectorMultiplyScalar_ReturnsMultipliedVector(double[] components, double scalar)
     {
-        var correctVector = new Vector(3)
-        {
-            [0] = component1 * scalar,
-            [1] = component2 * scalar,
-            [2] = component3 * scalar
-        };
+        var testVector = new Vector(components);
 
-        var testVector = new Vector(3)
+        for (var i = 0; i < components.Length; i++)
         {
-            [0] = component1,
-            [1] = component2,
-            [2] = component3
-        };
+            components[i] *= scalar;
+        }
+
+        var correctVector = new Vector(components);
 
         testVector.MultiplyByScalar(scalar);
 

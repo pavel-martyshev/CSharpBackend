a3b922cef7dcb4944b2c7be4c648fd929331483c
Pavel <pasha.martyshev@gmail.com>
2025-06-28 13:14:47 +0300

    Updated TransactionTask

--- TransactionTask/Program.cs
+++ TransactionTask/Program.cs
@@ -4,12 +4,12 @@ namespace TransactionTask
 {
     internal class Program
     {
-        private const string ConnectionString =
-            "Data Source=.;Initial Catalog=Shop;Integrated Security=true;Encrypt=False";
+        private const string ConnectionString = "Data Source=.;Initial Catalog=Shop;Integrated Security=true;Encrypt=False";
 
         public static void Main(string[] args)
         {
             using var connection = new SqlConnection(ConnectionString);
+
             connection.Open();
 
             var transaction = connection.BeginTransaction();
@@ -17,43 +17,39 @@ namespace TransactionTask
             try
             {
                 const string sql = """
-                                   INSERT INTO Categories (Name)
-                                   VALUES (N'Smartphones')
+                                   INSERT INTO Products (Name, Price, CategoryId)
+                                   VALUES (N'Asus', 4599, 1)
                                    """;
 
-                using (var command = new SqlCommand(sql, connection))
-                {
-                    command.Transaction = transaction;
-                    command.ExecuteNonQuery();
-                }
+                using var command = new SqlCommand(sql, connection);
+                command.Transaction = transaction;
+                command.ExecuteNonQuery();
+
+                throw new InvalidOperationException("Что-то пошло не так при вставке с транзакцией.");
 
                 transaction.Commit();
             }
-            catch (SqlException ex)
+            catch (Exception ex)
             {
                 transaction.Rollback();
-                Console.WriteLine(ex);
+                Console.WriteLine($"Ошибка вставки данных:{Environment.NewLine}{ex}");
             }
 
             try
             {
                 const string sql = """
-                                   INSERT INTO Categories (Name)
-                                   VALUES (N'Smartphones')
+                                   INSERT INTO Products (Name, Price, CategoryId)
+                                   VALUES (N'Not Asus', 999999, 1)
                                    """;
 
-                using (var command = new SqlCommand(sql, connection))
-                {
-                    command.Transaction = transaction;
-                    command.ExecuteNonQuery();
-                }
+                using var command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
 
-                transaction.Commit();
+                throw new InvalidOperationException("Что-то пошло не так при вставке без транзакции.");
             }
-            catch (SqlException ex)
+            catch (Exception ex)
             {
-                transaction.Rollback();
-                Console.WriteLine(ex);
+                Console.WriteLine($"Ошибка вставки данных:{Environment.NewLine}{ex}");
             }
         }
     }
